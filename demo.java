e9616986aeaa39e929e8115f707efa4ce526eb6a
mohanburidi <mohanburidi@gmail.com>
2025-04-10 16:08:12 +0530

    test commit 2

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 // Your First Program
 
-class HelloWorld good morning everyone {
+class HelloWorld good morning everyone all {
     public static void main(String[] args) {
-        System.out.println("Hello, World,good morning, everyone!"); 
+        System.out.println("Hello, World,good morning, everyone, all!"); 
     }
 }
